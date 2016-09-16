661a18417fd6e353612410b829ea684a7804731e
rudes <jacobriddle94@gmail.com>
2016-09-16 11:30:04 -0400

    Test doesn't complete due to non-existant dir

--- statimage/fonter_test.go
+++ statimage/fonter_test.go
@@ -11,6 +11,7 @@ import (
 // TestNewRuneStat tests the encoding of an image with a real players stats
 func TestNewRuneStat(t *testing.T) {
 	_staticDir := os.Getenv("GOPATH") + "/src/github.com/rudes/runestats/templates/static/"
+	os.MkdirAll(_staticDir+"images/os_rs/", os.ModeDir)
 	player := "peonpower"
 	stats := statapi.OldSchoolAPIHandler(player)
 	statimage.NewRuneStat(player, stats, _staticDir)
