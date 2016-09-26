70850f3f1e046e9b5c691817023bcd459e33cecb
rudes <jacobriddle94@gmail.com>
2016-09-25 21:58:05 -0400

    Moving to mocked API Call

--- statapi/parser_test.go
+++ statapi/parser_test.go
@@ -3,10 +3,42 @@ package statapi
 import (
 	"reflect"
 	"testing"
+
+	"github.com/jarcoal/httpmock"
 )
 
 // TestOldSchoolAPIHandler tests the data gathering from the OldSchool RS API
 func TestOldSchoolAPIHandler(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	httpmock.RegisterResponder("GET", _oldSchoolAPIURL+"peonpower",
+		httpmock.NewStringResponder(200, `498294,798,2963352
+		671596,48,89593
+		587194,44,55936
+		569143,60,273804
+		498668,63,378167
+		392395,67,582704
+		429100,44,56563
+		458370,59,267849
+		497653,48,86819
+		422761,57,203525
+		-1,1,-1
+		468465,52,123800
+		165045,55,166960
+		173438,61,316441
+		314849,50,101617
+		245290,59,252715
+		-1,1,-1
+		-1,1,-1
+		-1,1,-1
+		-1,1,-1
+		-1,1,-1
+		334553,23,6859
+		-1,1,-1
+		-1,1,-1
+		-1,-1
+		-1,-1
+		-1,-1`))
 	var expected []string
 	expected = append(expected, "798")
 	expected = append(expected, "48")
